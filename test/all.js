5fb21a842eac9351bdc73e1358fd4f406ff131be
mb1069 <mdb@ebi.ac.uk>
2017-09-25 11:26:31 +0100

    Removed broken package-lock history.

---
+++ test/all.js
@@ -0,0 +1,16 @@
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('request');
+var httpUtils = require('request-mocha')(request);
+
+describe('A server receiving a request', function () {
+
+    // Make request and save results to `this.err`, `this.res`, and `this.body`
+    httpUtils.save('http://localhost:8080/');
+
+    // Assert against mocha's `this` context
+    it('Server is online.', function () {
+        expect(this.err).to.equal(null);
+        expect(this.res.statusCode).to.equal(200);
+    });
+});
