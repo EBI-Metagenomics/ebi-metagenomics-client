4afaabc2159b5a35f0e40810e6bda6bb533abdbb
Miguel Boland <mdb@ebi.ac.uk>
2017-12-04 17:22:04 +0000

    Fixed URL in test config

--- config/test.js
+++ config/test.js
@@ -1,4 +1,4 @@
 var Config = module.exports = {
-    API_URL: "https://www.ebi.ac.uk/metagenomics/api/latest/",
+    API_URL: "http://127.0.0.1:8000/metagenomics/api/latest/",
     SEARCH_URL: "https://www.ebi.ac.uk/ebisearch/ws/rest/metagenomics_",
 };
