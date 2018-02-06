0133c14efe953de2463b95da13c4fff75399db0f
Miguel Boland <mdb@ebi.ac.uk>
2018-02-06 13:28:59 +0000

    Travis fix

--- cypress/integration/config.js
+++ cypress/integration/config.js
@@ -1,5 +1,6 @@
 var Config = module.exports = {
-    BASE_URL: 'http://localhost:8000/metagenomics/',
+    BASE_URL: Cypress.env('CLIENT_URL'),
+    // BASE_URL: 'http://localhost:8000/metagenomics/',
     API_URL: "http://localhost:8000/v1/",
     SEARCH_URL: "https://www.ebi.ac.uk/ebisearch/ws/rest/metagenomics_",
     INTERPRO_URL: "http://www.ebi.ac.uk/interpro/",
