dee85397531bf51c52ac714f6fc60057de09d668
Miguel Boland <mdb@ebi.ac.uk>
2018-03-01 14:55:14 +0000

    Added publication data to study

--- src/js/modules/study.js
+++ src/js/modules/study.js
@@ -6,8 +6,6 @@ const Pagination = require('../components/pagination').Pagination;
 const GenericTable = require('../components/genericTable');
 const Map = require('../components/map');
 const API_URL = process.env.API_URL;
-const EUROPEPMC_ENTRY_URL = Commons.EBI_EUROPEPMC_ENTRY_URL;
-const ENA_VIEW_URL = Commons.EBI_ENA_VIEW_URL;
 
 
 // const OverlappingMarkerSpiderfier = require('../../../static/libraries/oms.min.js');
@@ -15,17 +13,12 @@ import 'js-marker-clusterer';
 
 import {
     attachTabHandlers,
-    getURLFilterParams,
     getURLParameter,
-    hideTableLoadingGif,
-    initTableTools,
     setCurrentTab,
-    setURLParams,
-    showTableLoadingGif,
     createListItem,
     createLinkTag,
     checkURLExists,
-    checkAPIonline, getBiomeIcon, formatDate, formatLineage
+    checkAPIonline
 } from "../util";
 
 checkAPIonline();
@@ -44,25 +37,16 @@ let StudyView = Backbone.View.extend({
         const that = this;
         const deferred = $.Deferred();
         this.model.fetch({
-            data: $.param({}),
+            data: $.param({
+                include: 'publications'
+            }),
             success: function (data, response) {
-                const attr = data.attributes;
+                const pubObj = new api.Publication();
+                const publications = _.map(response.included, function(d){
+                    return new pubObj.parse(d);
+                });
+                that.model.attributes.publications = publications;
 
-                getExternalLinks(attr.id, attr.bioproject).done(function (data) {
-                    const links = _.map(data, function (url, text) {
-                        return createListItem(createLinkTag(url, text));
-                    });
-                    console.log("Check 2");
-                    that.model.attributes.external_links = links;
-                    console.log("Check 3");
-                });
-                getEuropePMCLinks(attr.id).done(function (data) {
-                    console.log("Check 4");
-                    const links = _.map(data, function (url, text) {
-                        return createListItem(createLinkTag(url, text));
-                    });
-                    that.model.attributes.europepmc_links = links;
-                });
                 that.$el.html(that.template(that.model.toJSON()));
                 deferred.resolve(true);
                 attachTabHandlers();
@@ -202,64 +186,6 @@ let RunsView = Backbone.View.extend({
     }
 });
 
-function getEuropePMCLinks(study_id) {
-    let deferred = new $.Deferred();
-    let publications = new StudyPublications({study_accession: study_id});
-    const publicationArray = [];
-
-    publications.fetch({
-        success: function (data, response) {
-            for (let i = 0; i < data.models.length; i++) {
-                const pubmed_id = data.models[i].attributes['pubmed_id'];
-                const pub_title = data.models[i].attributes['pub_title'];
-                const authors = data.models[i].attributes['authors'];
-                const doi = data.models[0].attributes['doi'];
-                publicationArray.push(new PublicationLink(pub_title, authors, pubmed_id, doi));
-            }
-            console.log("Check 5");
-        },
-        error: function () {
-            console.log("Couldn't fetch study publications!");
-        }
-    });
-
-    for (let i = 0; i < publicationArray.length; i++) {
-        const pubmed_id = publicationArray[i].pubmed_id;
-        const url = EUROPEPMC_ENTRY_URL + publicationArray[i].pubmed_id;
-        const url_check = checkURLExists(url);
-        console.log("Check 1");
-        let urls = {};
-        $.when(
-            url_check
-        ).done(function () {
-            if (url_check.status === 200) {
-                urls['PMID (' + pubmed_id + ')'] = url;
-            }
-            deferred.resolve(urls);
-
-        });
-    }
-    return deferred.promise();
-}
-
-function getExternalLinks(study_id, study_accession) {
-    var deferred = new $.Deferred();
-    const ena_url = ENA_VIEW_URL + study_accession;
-    const ena_url_check = checkURLExists(ena_url);
-    let urls = {};
-    $.when(
-        ena_url_check
-    ).done(function () {
-        if (ena_url_check.status === 200) {
-            urls['ENA website (' + study_id + ')'] = ena_url;
-        }
-        deferred.resolve(urls);
-
-    });
-    return deferred.promise();
-}
-
-
 // Called by googleMaps import callback
 function initPage() {
     let study = new api.Study({id: study_id});
@@ -271,7 +197,6 @@ function initPage() {
     let runs = new api.RunCollection({study_accession: study_id});
     let runsView = new RunsView({collection: runs});
 
-
     $.when(
         studyView.fetchAndRender(),
     ).done(function () {
@@ -280,44 +205,6 @@ function initPage() {
         new MapData(study_id).fetch();
     });
 }
-
-
-class PublicationLink {
-    constructor(pub_title, authors, pubmed_id, doi) {
-        this.pub_title = pub_title;
-        this.authors = authors;
-        this.pubmed_id = pubmed_id;
-        this.doi = doi;
-    }
-}
-
-const Publication = Backbone.Model.extend({
-    parse: function (d) {
-        const data = d.data !== undefined ? d.data : d;
-        const attrs = data.attributes;
-        return {
-            pubmed_id: attrs['pubmed-id'],
-            pub_title: attrs['pub-title'],
-            authors: attrs['authors'],
-            doi: attrs['doi'],
-        }
-    }
-});
-
-const StudyPublications = Backbone.Collection.extend({
-    model: Publication,
-    initialize: function (data) {
-        if (data && data.hasOwnProperty('study_accession')) {
-            this.study_accession = data.study_accession;
-        }
-    },
-    url: function () {
-        return API_URL + 'studies/' + this.study_accession + '/publications?page_size=10';
-    },
-    parse: function (response) {
-        return response.data;
-    }
-});
 
 
 window.initPage = initPage;
