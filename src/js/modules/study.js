1f88240393629893a1008dcffa4a6d20905d1d8c
Miguel Boland <mdb@ebi.ac.uk>
2018-04-09 09:12:35 +0100

    Removed duplication from study.js, improved linting rules to exclude some library functions

--- src/js/modules/study.js
+++ src/js/modules/study.js
@@ -18,14 +18,14 @@ let StudyView = Backbone.View.extend({
     model: api.Study,
     template: _.template($('#studyTmpl').html()),
     el: '#main-content-area',
-    fetchAndRender: function() {
+    fetchAndRender() {
         const that = this;
         const deferred = $.Deferred();
         this.model.fetch({
             data: $.param({
-                include: 'publications',
+                include: 'publications'
             }),
-            success: function(data, response) {
+            success(ignored, response) {
                 const pubObj = new api.Publication();
                 that.model.attributes.publications = _.map(response.included, function(d) {
                     return pubObj.parse(d);
@@ -35,23 +35,55 @@ let StudyView = Backbone.View.extend({
                 util.attachTabHandlers();
 
                 deferred.resolve(true);
-            },
+            }
         });
         return deferred.promise();
-    },
+    }
 });
+
+/**
+ * Method to update Samples or Runs view from pagination event
+ * @param view Backbone view for sample or studies
+ * @param page current page number
+ * @param pageSize size of current page
+ * @param order ordering string
+ * @param query filtering string
+ */
+function updateTableFromPagination(view, page, pageSize, order, query) {
+    view.tableObj.showLoadingGif();
+    let params = {
+        study_accession: view.collection.study_accession,
+        page: page,
+        page_size: pageSize
+    };
+    if (order) {
+        params['ordering'] = order;
+    }
+    if (query) {
+        params['search'] = query;
+    }
+    const that = view;
+    view.fetchXhr = view.collection.fetch({
+        data: $.param(params),
+        success: function(data, response) {
+            that.renderData(page, pageSize, response.meta.pagination.count,
+                response.links.first);
+            that.tableObj.hideLoadingGif();
+        }
+    });
+}
 
 let SamplesView = Backbone.View.extend({
     tableObj: null,
     pagination: null,
 
-    initialize: function() {
+    initialize() {
         const that = this;
         const columns = [
             {sortBy: 'sample_name', name: 'Sample name'},
             {sortBy: 'accession', name: 'Sample ID'},
             {sortBy: null, name: 'Description'},
-            {sortBy: 'last_update', name: 'Last update'},
+            {sortBy: 'last_update', name: 'Last update'}
         ];
         this.tableObj = new GenericTable($('#samples-section'), 'Associated samples', columns,
             Commons.DEFAULT_PAGE_SIZE_SAMPLES, false, function(page, pageSize, order, query) {
@@ -60,31 +92,11 @@ let SamplesView = Backbone.View.extend({
         this.update(1, Commons.DEFAULT_PAGE_SIZE_SAMPLES, null, null);
     },
 
-    update: function(page, pageSize, order, query) {
-        this.tableObj.showLoadingGif();
-        let params = {
-            study_accession: this.collection.study_accession,
-            page: page,
-            page_size: pageSize,
-        };
-        if (order) {
-            params['ordering'] = order;
-        }
-        if (query) {
-            params['search'] = query;
-        }
-        const that = this;
-        this.fetchXhr = this.collection.fetch({
-            data: $.param(params),
-            success: function(data, response) {
-                that.renderData(page, pageSize, response.meta.pagination.count,
-                    response.links.first);
-                that.tableObj.hideLoadingGif();
-            },
-        });
+    update(page, pageSize, order, query) {
+        updateTableFromPagination(this, page, pageSize, order, query);
     },
 
-    renderData: function(page, pageSize, resultCount, requestURL) {
+    renderData(page, pageSize, resultCount, requestURL) {
         const tableData = _.map(this.collection.models, function(m) {
             const attr = m.attributes;
             const sampleLink = '<a href=\'' + attr.sample_url + '\'>' + attr.sample_accession +
@@ -92,11 +104,11 @@ let SamplesView = Backbone.View.extend({
             return [attr.sample_name, sampleLink, attr.sample_desc, attr.last_update];
         });
         this.tableObj.update(tableData, true, page, pageSize, resultCount, requestURL);
-    },
+    }
 });
 
 let MapData = api.StudyGeoCoordinates.extend({
-    fetchAll: function() {
+    fetchAll() {
         this.data = [];
         const that = this;
         this.fetch({
@@ -112,24 +124,24 @@ let MapData = api.StudyGeoCoordinates.extend({
                     new Map('map', that.data, true);
                 }
             },
-            error: function(a, b, c) {
-            },
+            error: function() {
+            }
         });
-    },
+    }
 });
 
 let RunsView = Backbone.View.extend({
     tableObj: null,
     pagination: null,
 
-    initialize: function() {
+    initialize() {
         const that = this;
         const columns = [
             {sortBy: 'accession', name: 'Run ID'},
             {sortBy: null, name: 'Experiment type'},
             {sortBy: null, name: 'Instrument model'},
             {sortBy: null, name: 'Instrument platform'},
-            {sortBy: null, name: 'Pipeline versions'},
+            {sortBy: null, name: 'Pipeline versions'}
         ];
         this.tableObj = new GenericTable($('#runs-section'), 'Associated runs', columns,
             Commons.DEFAULT_PAGE_SIZE, false, function(page, pageSize, order, query) {
@@ -138,31 +150,11 @@ let RunsView = Backbone.View.extend({
         this.update(1, Commons.DEFAULT_PAGE_SIZE, null, null);
     },
 
-    update: function(page, pageSize, order, query) {
-        this.tableObj.showLoadingGif();
-        let params = {
-            study_accession: this.collection.study_accession,
-            page: page,
-            page_size: pageSize,
-        };
-        if (order) {
-            params['ordering'] = order;
-        }
-        if (query) {
-            params['search'] = query;
-        }
-        const that = this;
-        this.collection.fetch({
-            data: $.param(params),
-            success: function(data, response) {
-                that.renderData(page, pageSize, response.meta.pagination.count,
-                    response.links.first);
-                that.tableObj.hideLoadingGif();
-            },
-        });
+    update(page, pageSize, order, query) {
+        updateTableFromPagination(this, page, pageSize, order, query);
     },
 
-    renderData: function(page, pageSize, resultCount, requestURL) {
+    renderData(page, pageSize, resultCount, requestURL) {
         const tableData = _.map(this.collection.models, function(m) {
             const attr = m.attributes;
             const runLink = '<a href=\'' + attr.run_url + '\'>' + attr.run_id + '</a>';
@@ -174,22 +166,22 @@ let RunsView = Backbone.View.extend({
                 attr['pipeline_versions'].join(', ')];
         });
         this.tableObj.update(tableData, true, page, pageSize, resultCount, requestURL);
-    },
+    }
 });
 
 let DownloadsView = Backbone.View.extend({
     model: api.StudyDownloads,
     template: _.template($('#downloadsTmpl').html()),
     el: '#downloads',
-    initialize: function() {
+    initialize() {
         const that = this;
         this.model.fetch({
-            success: function(response, data) {
+            success: function(response) {
                 const pipelineFiles = response.attributes.pipelineFiles;
                 that.$el.html(that.template({pipeline_files: pipelineFiles}));
-            },
+            }
         });
-    },
+    }
 });
 
 /**
