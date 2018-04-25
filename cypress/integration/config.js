01f04eb988942402bf059dadfd45248995e85cfd
Miguel Boland <mdb@ebi.ac.uk>
2018-04-25 17:07:13 +0100

    Amended cypress config

--- cypress/integration/config.js
+++ cypress/integration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    BASE_URL: 'http://localhost:9000/metagenomics/',
-    API_URL: 'http://localhost:9000/metagenomics/api/v1/',
+    BASE_URL: 'http://127.0.0.1:9000/metagenomics/',
+    API_URL: 'http://127.0.0.1:9000/metagenomics/api/v1/',
     SEARCH_URL: 'https://www.ebi.ac.uk/ebisearch/ws/rest/metagenomics_',
     INTERPRO_URL: 'http://www.ebi.ac.uk/interpro/',
     SEQUENCE_SEARCH_URL: 'https://www.ebi.ac.uk/metagenomics/sequence-search/search/phmmer',
