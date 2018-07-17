e2d30bb78a89b066ac34f060c3ece77c3572454e
Miguel Boland <mdb@ebi.ac.uk>
2018-07-17 15:14:38 +0900

    Added error catching for 5xx responses from API with HTML error pages

--- src/js/modules/analysis.js
+++ src/js/modules/analysis.js
@@ -44,6 +44,10 @@ function loadReadLengthDisp(analysisID, statsData) {
     seqLengthData.fetch({
         dataType: 'text',
         success(ignored, response) {
+            if (typeof(response) === 'string') {
+                console.log('Could not load ReadLengthDisp data');
+                return;
+            }
             SeqLengthChart.drawSequenceLengthHistogram('readsLengthHist', response, false,
                 statsData,
                 seqLengthData.url());
@@ -62,6 +66,10 @@ function loadGCDistributionDisp(analysisID, statsData) {
     gcDistributionData.fetch({
         dataType: 'text',
         success(ignored, response) {
+            if (typeof(response) === 'string') {
+                console.log('Could not load ReadsGCHist data');
+                return;
+            }
             GCDispChart.drawSequenceGCDistribution('readsGCHist', response, false, statsData,
                 gcDistributionData.url());
             GCDispChart.drawGCContent('readsGCBarChart', statsData);
@@ -79,6 +87,10 @@ function loadNucleotideDisp(analysisID) {
     nucleotideDistData.fetch({
         dataType: 'text',
         success(ignored, response) {
+            if (typeof(response) === 'string') {
+                console.log('Could not load nucleotide data');
+                return;
+            }
             NucleotideChart.drawNucleotidePositionHistogram('nucleotide', response, false,
                 nucleotideDistData.url());
         }
@@ -202,7 +214,7 @@ function clusterData(data, depth) {
         const attr = d.attributes;
         let lineage = attr.lineage.split(':');
         // Remove empty strings
-        lineage = lineage.filter((v)=>v!=='');
+        lineage = lineage.filter((v) => v !== '');
         let category;
         if (lineage.length < depth) {
             category = lineage[lineage.length - 1];
