44ffa811eee0840ebac35cf4c44b18fdec0eb2ea
Miguel Boland <mdb@ebi.ac.uk>
2018-07-23 14:34:21 +0100

    Simplified nav tests to remove redundant testing

--- cypress/integration/nav.js
+++ cypress/integration/nav.js
@@ -4,26 +4,24 @@ const pageTitles = [
     'About', 'Help'];
 import {openPage, getPageURL} from '../util/util';
 
+const origPage = 'overview';
 /**
  * Check all links in navbar towards other pages of the site are functional
  */
 describe('Navbar test', function() {
-    for (let orig = 0; orig < navNames.length; orig++) {
-        for (let dest = 1; dest < navNames.length; dest++) {
-            const origPage = navNames[orig];
-            const destPage = navNames[dest];
-            context(origPage + '->' + destPage, function() {
-                it('Navbar link is valid.', function() {
-                    openPage(origPage);
-                    cy.get('#' + destPage + '-nav').click();
+    for (let dest = 1; dest < navNames.length; dest++) {
+        const destPage = navNames[dest];
+        context(origPage + '->' + destPage, function() {
+            it('Navbar link is valid.', function() {
+                openPage(origPage);
+                cy.get('#' + destPage + '-nav').click();
 
-                    if (origPage !== 'overview') {
-                        cy.url().should('include', destPage);
-                    }
-                    cy.get('h2').should('contain', pageTitles[dest]);
-                });
+                if (origPage !== 'overview') {
+                    cy.url().should('include', destPage);
+                }
+                cy.get('h2').should('contain', pageTitles[dest]);
             });
-        }
+        });
     }
 });
 
@@ -32,17 +30,14 @@ describe('Navbar test', function() {
  */
 const testQuery = 'testQuery';
 describe('Search bar redirection', function() {
-    for (let orig = 0; orig < navNames.length; orig++) {
-        const origPage = navNames[orig];
-        context(origPage + ' - Search redirects correctly', function() {
-            it('Navbar search re-directed correctly.', function() {
-                openPage(origPage);
-                cy.get('#headerSearchForm > input').type(testQuery);
-                cy.get('#search').click();
-                cy.url().should('include', 'search?query=' + testQuery);
-            });
+    context(origPage + ' - Search redirects correctly', function() {
+        it('Navbar search re-directed correctly.', function() {
+            openPage(origPage);
+            cy.get('#headerSearchForm > input').type(testQuery);
+            cy.get('#search').click();
+            cy.url().should('include', 'search?query=' + testQuery);
         });
-    }
+    });
 });
 const pagesBreadcrumbs = {
     'about': [''],
