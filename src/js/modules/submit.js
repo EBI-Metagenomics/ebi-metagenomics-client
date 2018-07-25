0d30414e37c246dcf6118aabd5db1099e8e3d19e
Miguel Boland <mdb@ebi.ac.uk>
2018-07-25 11:57:22 +0100

    Fixed consent status display

--- src/js/modules/submit.js
+++ src/js/modules/submit.js
@@ -8,6 +8,11 @@ const $actionDiv = $('#action');
 const userData = new authApi.UserDetails();
 const fetch = userData.fetch();
 
+/**
+ * General mailto url for give consent button
+ * @param {object} userData description object
+ * @return {string} href
+ */
 function getMailToLink(userData) {
     let body = 'I consent for the MGnify team to analyse the private data of my account ' +
         util.getUsername() + '.';
@@ -15,13 +20,21 @@ function getMailToLink(userData) {
     return 'mailto:metagenomics-help@ebi.ac.uk?subject=Request consent&body=' + body;
 }
 
+/**
+ * Displays relevant UI if user is not logged in
+ */
+function notLoggedInCallBack() {
+    const $button = $(util.getLoginLink('Please click here to login'));
+    $button.attr({'class': 'button'});
+    $actionDiv.html($button);
+}
+
 fetchLogin.done(function(loggedIn) {
     if (!loggedIn) {
-        const $button = $(util.getLoginLink('Please click here to login'));
-        $button.attr({'class': 'button'});
-        $actionDiv.html($button);
+        notLoggedInCallBack();
     } else {
-        fetch.done(function() {
+        fetch.always(function() {
+            console.log(userData, fetch);
             if (userData.attributes['analysis'] !== true) {
                 const $button = $('<button class=\'button\'>Give consent.</button>');
                 $button.attr('href', getMailToLink(userData.attributes));
@@ -43,4 +56,3 @@ fetchLogin.done(function(loggedIn) {
         });
     }
 });
-
