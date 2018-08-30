625c00514572d37869ce52c1fe804151def05e90
Miguel Boland <mdb@ebi.ac.uk>
2018-08-30 09:57:59 +0100

    Added tests for situation where user already consented to analysis

--- cypress/integration/submit.js
+++ cypress/integration/submit.js
@@ -1,4 +1,4 @@
-import {openPage, loginModal, fillLoginModalForm} from '../util/util';
+import {openPage, isValidLink} from '../util/util';
 
 const origPage = 'submit';
 describe('Submit page', function() {
@@ -11,16 +11,12 @@ describe('Submit page', function() {
             cy.contains('Please click here to login').should('be.visible');
         });
     });
-    context('User is logged in', function() {
+    context('User is logged in, consent not already given', function() {
         beforeEach(function() {
             cy.server();
             cy.route('GET', '**/myaccounts', 'fixture:user_account');
             openPage(origPage);
             cy.contains('Submit data').should('be.visible');
-            // cy.contains('Please click here to login').should('be.visible');
-            // cy.contains('Please click here to login').click();
-            // cy.get(loginModal).should('be.visible');
-            // fillLoginModalForm();
         });
         it('Should display consent button if logged in but consent not given', function() {
             cy.contains('Give consent').should('be.visible');
@@ -41,7 +37,7 @@ describe('Submit page', function() {
             cy.get('#consent-request-error').should('be.visible');
             cy.get('#consent-request-success').should('be.hidden');
         });
-        it('Should display success message if consent request failed', function() {
+        it('Should display success message on successful request', function() {
             cy.route({
                 method: 'POST',
                 url: '**',
@@ -56,6 +52,23 @@ describe('Submit page', function() {
             cy.contains('Give consent.').click();
             cy.get('#consent-request-error').should('be.hidden');
             cy.get('#consent-request-success').should('be.visible');
+
+            // Check error link validity
+            cy.get('#consent-request-success a').then(($el) => {
+                isValidLink($el);
+            });
+        });
+    });
+    context('User is logged in, consent already given', function() {
+        beforeEach(function() {
+            cy.server();
+            cy.route('GET', '**/myaccounts', 'fixture:user_account_with_consent');
+            openPage(origPage);
+            cy.contains('Submit data').should('be.visible');
+        });
+        it('Should display submit data button', function() {
+            cy.contains('Give consent.').should('not.exist');
+            cy.contains('Click here to submit data').should('be.visible');
         });
     });
 });
