bdb084988b46036379eb1837ffe8af49959cc8e0
Martin Beracochea <mbc@ebi.ac.uk>
2020-01-14 14:39:24 +0000

    Fix the expand of "More publications"

--- src/js/modules/about.js
+++ src/js/modules/about.js
@@ -1,10 +1,8 @@
 const util = require('../util');
 util.setupPage('#about-nav');
 
-
 // Expand button handler, usage as follows
 // Button --> <a class='expand-button' for='hiddenId'>Text></a>
 // Hidden content <div id='hiddenId' class='hidden'>Content</div>
 
 util.attachExpandButtonCallback();
-
