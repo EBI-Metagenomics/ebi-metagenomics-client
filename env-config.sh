0d222bde61f22bf159ff1683a75f0cfbfa445094
Gustavo A. Salazar <46671268+gustavo-salazar@users.noreply.github.com>
2021-05-27 16:01:22 +0100

    Gh actions dj3 (#176)
    * github actions- tests
    
    * displaying summary chart while waiting for interpro results
    
    * cypress credentials
    
    * gh_actions fix genome tests
    
    * adding coverage
    
    * removing.travis
    
    * using emgapi@develop in gh_actions
    
    Co-authored-by: Martín Beracochea <mbc@ebi.ac.uk>

--- env-config.sh
+++ env-config.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-export API_URL="http://localhost:9000/metagenomics/api/latest/"
+export API_URL="http://localhost:8000/v1/"
 export SEARCH_URL="http://localhost:9000/ebisearch/ws/rest/metagenomics_"
 export SEARCH_CSV_URL="http://localhost:9000/metagenomics/api/v1/ebi-search-download/"
 export INTERPRO_URL="http://localhost:9000/interpro/"
 export SEQUENCE_SEARCH_URL="http://localhost:9000/metagenomics/sequence-search/search/phmmer"
 export ENA_URL="http://localhost:9000/ena/browser/view/"
 export DEPLOYMENT_SUBFOLDER="/metagenomics"
+export CYPRESS_WEBIN_USERNAME="Webin-000"
+export CYPRESS_WEBIN_PASSWORD="Webin-111"
 
 export GMAPS_API_KEY="XX"
