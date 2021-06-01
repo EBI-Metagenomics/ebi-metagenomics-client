6e6f2d9eef32e4120d63664b4eca956a82918063
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-06-01 12:53:03 +0100

    prettier

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 [![Testing](https://github.com/EBI-Metagenomics/ebi-metagenomics-client/actions/workflows/test.yml/badge.svg?branch=new-client)](https://github.com/EBI-Metagenomics/ebi-metagenomics-client/actions/workflows/test.yml)
 [![codecov](https://codecov.io/gh/EBI-Metagenomics/ebi-metagenomics-client/branch/new-client/graph/badge.svg?token=WyXvRIQvq8)](https://codecov.io/gh/EBI-Metagenomics/ebi-metagenomics-client)
 
- # ebi-metagenomics-client
+# ebi-metagenomics-client
 
- This is the source ocode for the future version of the Mgnify website
+This is the source ocode for the future version of the Mgnify website
 
 #### Requirements:
- - Node.js >=10.13.0
- 
+
+-   Node.js >=10.13.0
+
 #### How to setup your project?
+
 Export the following env variables in env-config.sh file. Adjust exports if need depending on whether you are in your dev or
 prod environment.
 
@@ -17,11 +19,13 @@ source env-config.sh
 ```
 
 ##### Run `npm install` in the project's main directory.
- Serve with own server, or use built-in dev system by calling 'npm run server:watch'' in main directory.
+
+Serve with own server, or use built-in dev system by calling 'npm run server:watch'' in main directory.
+
 ```
 export DEPLOYMENT_SUBFOLDER=“/metagenomics”;
 npm install
 
-npm run watch 
+npm run watch
 
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  // plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http://localhost:3000/dist/',
+        hotOnly: true,
+    },
+    // plugins: [new webpack.HotModuleReplacementPlugin()]
+}
