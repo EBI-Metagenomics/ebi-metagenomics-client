3384c8811962a783ba34e11f70b76e2be81ba483
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-07-23 11:24:51 +0100

    producing css files and minimizing the production bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,84 @@
 const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 9000,
-    hot: true,
-    contentBasePublicPath: '/metagenomics',
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/[hash][ext][query]',
+module.exports = function (env, options) {
+  const isEnvProduction = options.mode === 'production';
+  const isEnvDevelopment = options.mode === 'development';
+  return {
+    mode: isEnvProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    devtool: isEnvProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 9000,
+      hot: true,
+      contentBasePublicPath: '/metagenomics',
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    modules: [
-      path.resolve(__dirname),
-      path.resolve(__dirname, 'src'),
-      path.resolve(__dirname, 'public'),
-      'node_modules',
-    ],
-  },
-  output: {
-    filename: 'mgnify_bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-      title: 'MGnify - EBI',
-    }),
-  ],
+        {
+          test: /\.css$/,
+          use: [
+            isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/[hash][ext][query]',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      modules: [
+        path.resolve(__dirname),
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'public'),
+        'node_modules',
+      ],
+    },
+    output: {
+      filename: 'mgnify_bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: isEnvProduction
+        ? 'js/[name].[contenthash:8].js'
+        : 'js/bundle.js',
+      chunkFilename: isEnvProduction
+        ? 'js/[name].[contenthash:8].chunk.js'
+        : 'js/[name].chunk.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        title: 'MGnify - EBI',
+      }),
+      isEnvProduction &&
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[contenthash:8].css',
+          chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+        }),
+      isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
+    ].filter(Boolean),
+    optimization: {
+      minimize: isEnvProduction,
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
