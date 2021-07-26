ff3fad336360d32680dc0bc8650dd53b6b4fd5bf
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-07-26 10:26:45 +0100

    eslint fixes again

--- src/components/UI/InnerCard/index.tsx
+++ src/components/UI/InnerCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ArrowForLink from 'components/UI/ArrowForLink';
 import { Link } from 'react-router-dom';
+
 type InnerCardProps = {
   title: string;
   label: string;
@@ -9,7 +10,7 @@ type InnerCardProps = {
 
 const InnerCard: React.FC<InnerCardProps> = ({ title, label, to }) => {
   return (
-    <article className="vf-card vf-card--brand vf-card--striped vf-card--bordered">
+    <article className="vf-card vf-card--striped vf-card--bordered">
       <div className="vf-card__content | vf-stack vf-stack--400">
         <p className="vf-card__text">{label}</p>
         <h3 className="vf-card__heading">
