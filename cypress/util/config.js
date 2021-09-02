1e8e40775f4e6f1a006c823805345ac3674f8156
sandyr <sandyr@ebi.ac.uk>
2021-09-02 14:11:31 +0100

    add integration tests for catalogues genomes listing

--- cypress/util/config.js
+++ cypress/util/config.js
@@ -1,6 +1,6 @@
 const Config = {
     BASE_URL: 'http://localhost:9000/metagenomics/',
-    API_URL: process.env.API_URL || 'http://localhost:9000/metagenomics/api/v1/',
+    API_URL: process.env.API_URL || Cypress.env('API_URL') || 'http://localhost:9000/metagenomics/api/v1/',
     SEARCH_URL: 'https://wwwdev.ebi.ac.uk/ebisearch/ws/rest/metagenomics_',
     INTERPRO_URL: 'http://www.ebi.ac.uk/interpro/',
     SEQUENCE_SEARCH_URL: 'https://www.ebi.ac.uk/metagenomics/sequence-search/search/phmmer',
