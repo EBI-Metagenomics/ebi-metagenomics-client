cfced7ec22d9c88440e711af6295aef9c15c7893
sandyr <sandyr@ebi.ac.uk>
2021-10-21 16:32:32 +0100

    adds "additional metadata" section to sample page, detailing whether or not any annotations exist on associated studies' publications

--- src/js/components/europePMCAnnotations.js
+++ src/js/components/europePMCAnnotations.js
@@ -1,11 +1,13 @@
 const Backbone = require('backbone');
 const api = require('mgnify').api(process.env.API_URL);
 
-const tmpl = require('../../partials/europePMCAnnotations.handlebars');
+const publicationAnnotationTmpl = require('../../partials/europePMCAnnotations.handlebars');
+const sampleAnnotationsTmpl = require(
+    '../../partials/sampleAnnotationsFromStudiesPublications.handlebars');
 
 export const PublicationEuropePMCAnnotationsView = Backbone.View.extend({
     model: api.PublicationEuropePMCAnnotations,
-    template: tmpl,
+    template: publicationAnnotationTmpl,
     initialize({publicationId}) {
         this.$loadingSpinner = this.$('.loading-spinner');
         // eslint-disable-next-line new-cap
@@ -18,5 +20,26 @@ export const PublicationEuropePMCAnnotationsView = Backbone.View.extend({
         // this.model.set('pubmedID', this.model.id);
         that.$el.html(that.template(that.model.toJSON()));
         that.$loadingSpinner.hide();
+    }
+});
+
+export const SampleAdditionalMetadataFromStudiesPublicationsView = Backbone.View.extend({
+    model: api.SampleStudiesPublicationsAnnotationsExistence,
+    template: sampleAnnotationsTmpl,
+    initialize({sampleId}) {
+        // eslint-disable-next-line new-cap
+        this.model = new this.model({id: sampleId});
+        this.model.fetch();
+        this.model.on('sync', this.render, this);
+    },
+    render() {
+        const that = this;
+        if (this.model.attributes.query_possible &&
+            !this.model.attributes.studies_with_annotations.length) {
+            // no studies
+            that.$el.empty();
+            return;
+        }
+        that.$el.html(that.template(that.model.toJSON()));
     }
 });
