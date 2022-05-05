3ca08bad9ea8b2e0d8568856403e0d1ad899841e
sandyr <sandyr@ebi.ac.uk>
2022-05-05 17:04:28 +0100

    fetches instrument types from `runs` when viewing hybrid-assembly analyses

--- src/components/Analysis/Overview/index.tsx
+++ src/components/Analysis/Overview/index.tsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import KeyValueList from 'components/UI/KeyValueList';
 import { Link } from 'react-router-dom';
 import AnalysisContext from 'pages/Analysis/AnalysisContext';
+import useMGnifyData from 'hooks/data/useMGnifyData';
 
 function isAssembly(experimentType: string): boolean {
   return ['assembly', 'hybrid_assembly', 'long_reads_assembly'].includes(
@@ -9,9 +10,55 @@ function isAssembly(experimentType: string): boolean {
   );
 }
 
+type Run = {
+  attributes: {
+    'instrument-platform': string;
+    'instrument-model': string;
+  };
+};
+
+type HybridAnalysisDetailsProps = {
+  assemblyId: string;
+};
+
+const HybridAnalysisDetails: React.FC<HybridAnalysisDetailsProps> = ({
+  assemblyId,
+}) => {
+  const { loading: loadingRuns, data: runs } = useMGnifyData(
+    `assemblies/${assemblyId}/runs`
+  );
+  const instruments = useMemo(() => {
+    if (loadingRuns) return '';
+    const runsData = runs.data as Run[];
+    return runsData
+      .map(
+        (run) =>
+          `${run.attributes['instrument-platform']} – ${run.attributes['instrument-model']}`
+      )
+      .join('\n');
+  }, [loadingRuns, runs]);
+  return (
+    <KeyValueList
+      list={[
+        {
+          key: 'Experiment type',
+          value: 'Hybrid assembly',
+        },
+        {
+          key: 'Instruments',
+          value: instruments,
+        },
+      ].filter(({ value }) => !!value)}
+    />
+  );
+};
+
 const AnalysisOverview: React.FC = () => {
   const { overviewData: data } = useContext(AnalysisContext);
 
+  const isHybrid =
+    data.attributes['experiment-type'] === 'hybrid_assembly' &&
+    !!data.relationships.assembly.data;
   return (
     <section>
       <div className="vf-stack">
@@ -84,23 +131,29 @@ const AnalysisOverview: React.FC = () => {
           <summary>
             <b>Experiment details</b>
           </summary>
-          <KeyValueList
-            list={[
-              // TODO: the lists should obtained from runs for hybrid_assembly
-              {
-                key: 'Experiment type',
-                value: data.attributes['experiment-type'] as string,
-              },
-              {
-                key: 'Instrument model',
-                value: data.attributes['instrument-model'] as string,
-              },
-              {
-                key: 'Instrument platform',
-                value: data.attributes['instrument-platform'] as string,
-              },
-            ].filter(({ value }) => !!value)}
-          />
+          {!isHybrid && (
+            <KeyValueList
+              list={[
+                {
+                  key: 'Experiment type',
+                  value: data.attributes['experiment-type'] as string,
+                },
+                {
+                  key: 'Instrument model',
+                  value: data.attributes['instrument-model'] as string,
+                },
+                {
+                  key: 'Instrument platform',
+                  value: data.attributes['instrument-platform'] as string,
+                },
+              ].filter(({ value }) => !!value)}
+            />
+          )}
+          {isHybrid && (
+            <HybridAnalysisDetails
+              assemblyId={data.relationships.assembly.data.id}
+            />
+          )}
         </details>
       </div>
     </section>
