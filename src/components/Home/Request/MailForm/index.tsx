5ee04d296b2c64336c3d0a68fee0e8578accc949
Martin Beracochea <mbc@ebi.ac.uk>
2022-09-16 15:53:08 +0100

    Add a message and request additional info for Analysis&Assembly
    The changes capture extra information in the submission interface,
    aimed for the assembly step.

--- src/components/Home/Request/MailForm/index.tsx
+++ src/components/Home/Request/MailForm/index.tsx
@@ -49,6 +49,7 @@ const EMPTY_EMAIL = {
 };
 const MailForm: React.FC<MailFormProps> = ({ isPublic }) => {
   const [analysisType, setAnalysisType] = useState('Analysis');
+  const [rawReadsType, setRawType] = useState('');
   const [accession, setAccession] = useState('');
   const [comments, setComments] = useState('');
   const [result, setResult] = useState('');
@@ -111,6 +112,7 @@ const MailForm: React.FC<MailFormProps> = ({ isPublic }) => {
   };
   const handleClear = () => {
     setAnalysisType('Analysis');
+    setRawType('');
     setAccession('');
     setComments('');
     setResult('');
@@ -188,18 +190,73 @@ const MailForm: React.FC<MailFormProps> = ({ isPublic }) => {
               </label>
             </div>
           </div>
-          <div>
-            <label className="vf-text-heading--4">
-              Comment / additional information
-              <input
-                value={comments}
-                onChange={(event) => setComments(event.target.value)}
-                type="text"
-                className="vf-form__input"
-                name="reason"
-              />
-            </label>
+          <div className="vf-stack">
+            <div>
+                <label className="vf-text-heading--4">
+                  Are your raw reads
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="raw-reads-type"
+                    value="ShortReads"
+                    checked={rawReadsType === 'ShortReads'}
+                    onChange={() => setRawType('ShortReads')}
+                  />
+                  short-reads (e.g. Illumina)
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="raw-reads-type"
+                    value="LongReads"
+                    checked={rawReadsType === 'LongReads'}
+                    onChange={() => setRawType('LongReads')}
+                  />
+                  long-reads (e.g. Oxford Nanopore, PacBio SMRT)
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="raw-reads-type"
+                    value="ShortAndLongReads"
+                    checked={rawReadsType === 'ShortAndLongReads'}
+                    onChange={() => setRawType('ShortAndLongReads')}
+                  />
+                  a combination of both short-reads and long-reads for the same sample
+                </label>
+              </div>
           </div>
+        </div>
+        <div className="vf-grid">
+          <label className="vf-text-heading--4">
+            Comments / additional information
+            {(rawReadsType == "LongReads") ?
+                <p>
+                  Please provide details of the sequencing instrument and base-caller version.
+                </p>
+              : null
+            }
+            {(rawReadsType == "ShortAndLongReads") ?
+              <p>
+                Please provide details of the sequencing instrument and base-caller version used for long-reads.
+                If the long-reads and short-reads data are submitted into separate studies, please also provide the study/project ID(s) for the additional data.
+              </p>
+              : null
+            }
+            <textarea
+              className="vf-form__textarea"
+              name="reason"
+              onChange={(event) => setComments(event.target.value)}
+              value={comments}>
+            </textarea>
+          </label>
         </div>
         <div>
           <button
