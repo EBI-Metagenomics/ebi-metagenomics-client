e6194070af8083d511313083a08c191dcabcd103
Sandy Rogers <sandyr@ebi.ac.uk>
2023-01-09 09:15:33 +0000

    adds hook to redirect to the canonical accession of an object (e.g. study), where the API supports secondary accessions (#269)

---
+++ src/hooks/useCanonicalAccessionRedirect/index.tsx
@@ -0,0 +1,25 @@
+import useURLAccession from 'hooks/useURLAccession';
+import { MGnifyResponseObj } from 'hooks/data/useData';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const useCanonicalAccessionRedirect = (data: MGnifyResponseObj) => {
+  const urlAccession = useURLAccession();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  if (!data) return;
+  if (!urlAccession) return;
+
+  const dataAccession = data.data.id;
+
+  if (
+    dataAccession &&
+    dataAccession.toUpperCase() !== urlAccession.toUpperCase()
+  ) {
+    navigate(pathname.replace(urlAccession, dataAccession));
+  }
+};
+
+export default useCanonicalAccessionRedirect;
