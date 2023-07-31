54a5618d5433936d91d219b238704d84f3bef17b
Mahfouz Shehu <mahfouz@ebi.ac.uk>
2023-07-31 23:10:14 +0100

    WIP, implemeneting iframe mutation observer

--- src/components/IGV/ROCrateTrack/index.tsx
+++ src/components/IGV/ROCrateTrack/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import EMGModal from 'components/UI/EMGModal';
 import RoCrateSingleton from 'utils/roCrateSingleton';
 
@@ -15,6 +15,46 @@ const ROCratePreview: React.FC<ROCratePreviewProps> = ({
 }) => {
   const [cratePreview, setCratePreview] = useState('');
   const [crateModalOpen, setCrateModalOpen] = useState(false);
+  const myIframe = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    const iframe = myIframe.current as any;
+
+    // Define the MutationObserver callback function
+    const handleMutation = (mutationsList: MutationRecord[]) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const mutation of mutationsList) {
+        console.log('mutation', mutation);
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'srcDoc'
+        ) {
+          console.log(
+            'Location changed in iframe:',
+            (mutation.target as any).srcDoc
+          );
+          // Perform any actions based on the location change here
+        }
+      }
+    };
+
+    // Create a MutationObserver instance
+    const observer = new MutationObserver(handleMutation);
+    console.log(observer);
+    (window as any).observer = observer;
+
+    // Observe the 'src' attribute of the iframe
+    if (iframe) {
+      observer.observe(iframe, {
+        attributes: true,
+        // attributeFilter: ['srcDoc'],
+      });
+    }
+    // observer.observe(iframe, { attributes: true, attributeFilter: ['src'] });
+
+    // Clean up the observer when the component is unmounted
+    return () => observer.disconnect();
+  }, [myIframe.current]);
 
   function populateCratePreview() {
     RoCrateSingleton.getPreviewHtml(crateUrl, specificCrateFolder).then(
@@ -48,9 +88,12 @@ const ROCratePreview: React.FC<ROCratePreviewProps> = ({
         isOpen={crateModalOpen}
         onRequestClose={() => setCrateModalOpen(false)}
         contentLabel="RO-Crate preview modal"
+        iframeRef={myIframe}
       >
         <iframe
-          srcDoc={cratePreview}
+          id="iframe"
+          ref={myIframe}
+          srcDoc={`<!DOCTYPE html><html><head></head><body><a href="/fake.html" id="page2Anchor">HEllo</a></body></html>`}
           title="RO-Crate Preview"
           width="100%"
           height="100%"
--- src/components/UI/EMGModal/index.tsx
+++ src/components/UI/EMGModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactChild, ReactChildren } from 'react';
+import React, { ReactChild, ReactChildren, useEffect, useRef } from 'react';
 
 import Modal from 'react-modal';
 
@@ -30,19 +30,81 @@ type ModalProps = {
   onRequestClose: () => void;
   contentLabel: string;
   children: ReactChild | ReactChild[] | ReactChildren | ReactChildren[];
+  iframeRef?: React.RefObject<HTMLIFrameElement>;
 };
 const EMGModal: React.FC<ModalProps> = ({
   isOpen,
   onRequestClose,
   contentLabel,
   children,
+  iframeRef,
 }) => {
+  // useEffect(() => {
+  //   React.Children.forEach(children, (child) => {
+  //     // Access the props of each child component
+  //     if (React.isValidElement(child)) {
+  //       console.log('child', child);
+  //       console.log(child.props.srcDoc);
+  //       // console.log(child.ref.current);
+  //       // eslint-disable-next-line no-param-reassign
+  //       // child.props.srcDoc = '<h1>Hello</h1>';
+  //       // console.log(child['ref']);
+  //     }
+  //   });
+  // }, [children]);
+
+  // useEffect(() => {
+  //   // const iframe = document.getElementById('iframe');
+  //   console.log(children);
+  //   const newObj = children;
+  //   console.log(newObj.props);
+  //   // if (iframe) {
+  //   //   console.log(iframe);
+  //   // }
+  // }, [children]);
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel={contentLabel}
       style={modalStyle}
+      onAfterOpen={() => {
+        if (iframeRef && iframeRef.current) {
+          // console.log('iframeRef', iframeRef);
+          // console.log('iframeRef.current', iframeRef.current);
+          // console.log(
+          //   'iframeRef.current.contentWindow',
+          //   iframeRef.current.contentWindow
+          // );
+          // console.log(
+          //   'iframeRef.current.contentWindow.document',
+          //   iframeRef.current.contentWindow.document
+          // );
+          // console.log(
+          //   'iframeRef.current.contentWindow.document.body',
+          //   iframeRef.current.contentWindow.document.body
+          // );
+          // console.log(
+          //   'iframeRef.current.contentWindow.document.body.innerHTML',
+          //   iframeRef.current.contentWindow.document.body.innerHTML
+          // );
+
+          // eslint-disable-next-line no-param-reassign
+          iframeRef.current.contentWindow.document.onload = () => {
+            const iframePage2Anchor =
+              iframeRef.current.contentWindow.document.getElementById(
+                'page2Anchor'
+              );
+            iframePage2Anchor.addEventListener('click', (e) => {
+              e.preventDefault();
+              alert('iframePage2Anchor clicked');
+            });
+          };
+          console.log('current doc', document);
+          console.log(iframeRef.current.contentWindow.document);
+        }
+      }}
     >
       <div className="emg-modal-close">
         <button
