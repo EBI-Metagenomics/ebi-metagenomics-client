fa26416494dd02e9763d4eee485d8ff25834a7b1
Mahfouz <mahfouzshehu@gmail.com>
2023-08-11 10:37:09 +0100

    slight refactor to improve method naming in roCrateBrowser e2e test

--- cypress/e2e/ro_crate_browser.js
+++ cypress/e2e/ro_crate_browser.js
@@ -1,24 +1,24 @@
 describe("RO-Crate Browser", () => {
   it("Can browse an RO crate from the Contig viewer page", () => {
     tryToBrowseAnRoCrate("http://localhost:9000/metagenomics/analyses/MGYA00000001?selected_contig=ERZ8153470.8-NODE-8-length-192555-cov-12.276894#contigs-viewer", ($iframe) => {
-      sanntisIframeContentsVerifier($iframe);
+      verifySanntisCrateIframeContents($iframe);
     });
   });
 
   it("Can browse an RO crate from Assembly extra annotations", () => {
     tryToBrowseAnRoCrate("http://localhost:9000/metagenomics/assemblies/ERZ8153470", ($iframe) => {
-      sanntisIframeContentsVerifier($iframe);
+      verifySanntisCrateIframeContents($iframe);
     });
   });
 
   it('Can browse an RO crate from Run extra annotations', () => {
     tryToBrowseAnRoCrate('http://localhost:9000/metagenomics/runs/SRR8845517', ($iframe) => {
-      motusIframeContentsVerifier($iframe);
+      verifyMotusCrateIframeContents($iframe);
     });
   });
 });
 
-const tryToBrowseAnRoCrate = (location, iframeContentsVerifier) => {
+const tryToBrowseAnRoCrate = (location, verifyIframeContents) => {
   cy.visit(location);
   const roCrateBrowserButton = ".ro-crate-browser-button";
   // cy.get(".ro-crate-browser-button").should("exist");
@@ -27,17 +27,17 @@ const tryToBrowseAnRoCrate = (location, iframeContentsVerifier) => {
   cy.get(roCrateBrowserButton).last().click();
   cy.get("iframe").should("be.visible");
   cy.get("iframe").then($iframe => {
-    iframeContentsVerifier($iframe);
+    verifyIframeContents($iframe);
     cy.wait(1000);
     cy.get(".modal-close-button").click();
   });
 };
 
-const sanntisIframeContentsVerifier = ($iframe) => {
+const verifySanntisCrateIframeContents = ($iframe) => {
   cy.wrap($iframe.contents().find("a:contains(\"ERZ8153470.sanntis.gff\")")).should("not.be.empty");
 };
 
-const motusIframeContentsVerifier = ($iframe) => {
+const verifyMotusCrateIframeContents = ($iframe) => {
   cy.wrap($iframe.contents().find("a:contains(\"multiqc\")"))
     .should("not.be.empty")
     .then(($multiqcAnchor) => {
