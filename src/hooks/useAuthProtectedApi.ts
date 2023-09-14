fd2e15d04825398b152885819f547aab8e3d6066
Mahfouz <mahfouzshehu@gmail.com>
2023-09-14 14:32:37 +0100

    removed unused file

--- src/hooks/useAuthProtectedApi.ts
+++
@@ -1,9 +0,0 @@
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-//
-//
-// const apiUtils = axios.create({
-//   // You can set common configurations here, such as baseURL if needed
-// });
-//
-// export const useAuthProtectedApi = (url: string) => {
