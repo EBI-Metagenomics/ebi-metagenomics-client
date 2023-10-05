a10d5693ced937a9c8d752e11bfdbc1aff74d943
Mahfouz <mahfouzshehu@gmail.com>
2023-10-05 11:13:56 +0100

    useData hook adequately integrated with protectedAxiosCall

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:8000/v1';
-const BASE_URL = 'https://wwwint.ebi.ac.uk/metagenomics/api/v1';
+const BASE_URL = 'http://localhost:8000/v1';
+// const BASE_URL = 'https://wwwint.ebi.ac.uk/metagenomics/api/v1';
 export default axios.create({
   baseURL: BASE_URL,
 });
