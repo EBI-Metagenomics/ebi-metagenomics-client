10ab1d35368fa4dd48e3f4afb8170a10f3b092c8
Mahfouz <mahfouzshehu@gmail.com>
2023-10-26 13:22:14 +0100

    Testing to see if using localhost base url will avert CORS issues

--- src/utils/protectedAxios.ts
+++ src/utils/protectedAxios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://wwwint.ebi.ac.uk/metagenomics/api/v1';
+// const BASE_URL = 'https://wwwint.ebi.ac.uk/metagenomics/api/v1';
+const BASE_URL = 'http://localhost:8000/v1';
 
 const protectedAxios = axios.create({
   baseURL: BASE_URL,
