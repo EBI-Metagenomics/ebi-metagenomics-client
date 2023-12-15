87740214c2079879124644bc5c3c00648e6b7053
sandyr <sandyr@ebi.ac.uk>
2023-12-15 12:01:02 +0000

    switch to VF Framework version of elixir banner, CDR logo

--- src/components/UI/ElixirBanner/index.tsx
+++ src/components/UI/ElixirBanner/index.tsx
@@ -1,43 +1,18 @@
 import React from 'react';
+import { vfBannerElixir } from '@visual-framework/vf-banner-elixir/vf-banner-elixir';
 
 const ElixirBanner: React.FC = () => {
+  vfBannerElixir();
   return (
     <section className="vf-u-fullbleed | vf-u-background-color-ui--grey--light">
       <div
         className="vf-banner-elixir vf-banner"
         data-vf-js-banner-elixir=""
-        data-vf-js-banner-elixir-logo=""
+        data-vf-js-banner-elixir-logo="https://ebi.emblstatic.net/web_guidelines/EBI-Framework/v1.4/images/logos/ELIXIR/elixir-cdr.gif"
         data-vf-js-banner-elixir-name="MGnify"
-        data-vf-js-banner-elixir-description="The MGnify platform facilitates the assembly, analysis and archiving of microbiome-derived nucleic acid sequences. The platform provides access to taxonomic assignments and functional annotations for nearly half a million analyses covering metabarcoding, metatranscriptomic, and metagenomic datasets, which are derived from a wide range of different environments."
+        data-vf-js-banner-elixir-description="MGnify is an ELIXIR Core Data Resource"
         data-vf-js-banner-elixir-link="default"
-      >
-        <div className="vf-flag vf-flag--middle vf-flag--400">
-          <div className="vf-flag__media">
-            <a href="http://www.elixir-europe.org" className="vf-banner__link">
-              <img
-                src="https://ebi.emblstatic.net/web_guidelines/EBI-Framework/v1.2/images/logos/assorted/elixir_kitemark-60px.png"
-                alt="ELIXIR Logo"
-              />
-            </a>
-          </div>
-          <div className="vf-flag__body">
-            <h4 className="vf-banner__text--lg">
-              <a href="http://www.elixir-europe.org" className="vf-link">
-                MGnify is part of the ELIXIR infrastructure
-              </a>
-            </h4>
-            <p className="vf-banner__text">
-              MGnify is an ELIXIR Core Data Resource{' '}
-              <a
-                className="vf-banner__link"
-                href="https://elixir-europe.org/platforms/data/core-data-resources"
-              >
-                Learn more
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
+      />
     </section>
   );
 };
