0769070e803fd1f80d687de3a0a35498765da6d4
Mahfouz <mahfouzshehu@gmail.com>
2024-01-30 11:57:31 +0000

    Fixed linter errors

--- src/components/Study/Overview/index.tsx
+++ src/components/Study/Overview/index.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { Publication } from 'components/Publications';
 import AnalysesTable from 'components/Analysis/Analyses';
 import Box from 'components/UI/Box';
