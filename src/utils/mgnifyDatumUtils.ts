c58e45798e8b24b1a6dc7b5310dbf5d32c18f6ad
MGS-sails <mahfouzshehu@gmail.com>
2024-11-27 10:09:04 +0000

    WIP

---
+++ src/utils/mgnifyDatumUtils.ts
@@ -0,0 +1,14 @@
+export function createDefaultMGnifyDatum(): MGnifyDatum {
+  return {
+    study_accession: '',
+    run_accession: '',
+    sample_accession: '',
+    assembly_accession: '',
+    accession: '',
+    experiment_type: '',
+    instrument_model: '',
+    instrument_platform: '',
+    pipeline_version: '',
+    downloads: [],
+  };
+}
