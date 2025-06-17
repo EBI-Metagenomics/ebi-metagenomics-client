828060885e98889ad995d94e4b76071abff3d6c8
MGS-sails <mahfouzshehu@gmail.com>
2025-06-17 12:16:16 +0100

    Added impact survey banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,11 +5,27 @@ import BlogExcerpts from 'components/Home/BlogExcerpts';
 import Publications, { MainPublication } from 'components/Publications';
 import './style.css';
 import TrainingResources from 'components/Home/TrainingResources';
+import ExtLink from 'components/UI/ExtLink';
 
 const HomePage: React.FC = () => {
   return (
     <section className="vf-content vf-stack vf-stack--800">
       <div />
+      <div className="vf-banner vf-banner--alert vf-banner--info">
+        <div className="vf-banner__content">
+          <p className="vf-banner__text">
+            Do data resources managed by EMBL-EBI and our collaborators make a
+            difference to your work?
+            <br />
+            Please take 10 minutes to fill in our{' '}
+            <ExtLink href="https://www.surveymonkey.com/r/QGFMBH8?channel=[webpage]">
+              annual user survey
+            </ExtLink>
+            , and help us make the case for why sustaining open data resources
+            is critical for life sciences research.
+          </p>
+        </div>
+      </div>
       <div className="vf-grid vf-grid__col-2">
         <SearchBy />
         <Request />
