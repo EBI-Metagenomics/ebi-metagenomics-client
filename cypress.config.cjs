a6fb7b901147c47c4dcf1f0429680968dd4e914f
MGS-sails <mahfouzshehu@gmail.com>
2025-09-02 16:15:17 +0100

    Adjusted cypress config to conform to vite based approach

---
+++ cypress.config.cjs
@@ -0,0 +1,46 @@
+// cypress.config.cjs
+const { defineConfig } = require('cypress');
+const codeCoverageTask = require('@cypress/code-coverage/task');
+
+module.exports = defineConfig({
+  projectId: '8vc1o6',
+  chromeWebSecurity: false,
+  viewportWidth: 1200,
+  viewportHeight: 900,
+  video: false,
+  videoCompression: false,
+  watchForFileChanges: false,
+  defaultCommandTimeout: 40000,
+  retries: 3,
+  e2e: {
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    },
+    excludeSpecPattern: '*.disabled',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+});
+// import { defineConfig } from 'cypress';
+
+// import codeCoverageTask from '@cypress/code-coverage/task.js';
+// // import codeCoverageTask from '@cypress/code-coverage/task';
+//
+// export default defineConfig({
+//   projectId: '8vc1o6',
+//   chromeWebSecurity: false,
+//   viewportWidth: 1200,
+//   viewportHeight: 900,
+//   video: false,
+//   videoCompression: false,
+//   watchForFileChanges: false,
+//   defaultCommandTimeout: 40000,
+//   retries: 3,
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       return codeCoverageTask(on, config);
+//     },
+//     excludeSpecPattern: '*.disabled',
+//     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+//   },
+// });
