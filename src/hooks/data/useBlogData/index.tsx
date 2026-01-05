2e4944484d327c136958c2ea48ef25b6c25209f8
Sandy Rogers <sandyr@ebi.ac.uk>
2026-01-05 15:10:29 +0000

    do not send auth headers to blog

--- src/hooks/data/useBlogData/index.tsx
+++ src/hooks/data/useBlogData/index.tsx
@@ -1,16 +1,36 @@
-import { useContext } from 'react';
-import useData, { DataResponse, BlogResponse } from '@/hooks/data/useData';
+import { useContext, useEffect, useState } from 'react';
+import { BlogResponse } from '@/hooks/data/useData';
 import UserContext from 'pages/Login/UserContext';
 import { isNil } from 'lodash-es';
+import axios from 'axios';
+import { useBoolean } from 'react-use';
 
-interface BlogDataResponse extends DataResponse {
+interface BlogDataResponse {
   data: BlogResponse;
+  error?: any;
+  loading: boolean;
 }
 
 const useBlogData: (resource: string) => BlogDataResponse = (resource) => {
   const { config } = useContext(UserContext);
-  const data = useData(isNil(resource) ? null : `${config.blog}${resource}`);
-  return data as BlogDataResponse;
+  const [isLoading, setIsLoading] = useBoolean(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState<BlogResponse>({});
+
+  useEffect(() => {
+    if (isNil(resource)) return;
+    axios
+      .get(`${config.blog}${resource}`)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((axiosError) => setError(axiosError))
+      .finally(() => setIsLoading(false));
+  }, [config.blog, resource, setIsLoading]);
+
+  return { data, error, loading: isLoading } as BlogDataResponse;
 };
 
 export default useBlogData;
