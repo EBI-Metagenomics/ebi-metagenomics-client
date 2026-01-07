af6229754052252281e26384db862bb835dde873
MGS-sails <mahfouzshehu@gmail.com>
2026-01-07 22:20:55 +0000

    Removed debris

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
       name: 'copy-mgnify-component',
       writeBundle() {
         // Copy the component file to dist after build
+        // TODO, although not being used now, this code will be needed when we switch back to in browser sketching for Branchwater
         copyFileSync(
           resolve(
             __dirname,
@@ -94,34 +95,3 @@ export default defineConfig({
     port: 9000,
   },
 });
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd());
-//
-//   return {
-//     base: env.VITE_BASE_URL ?? '/metagenomics',
-//     plugins: [react()],
-//     resolve: {
-//       alias: {
-//         '@': path.resolve(__dirname, './src'),
-//         src: path.resolve(__dirname, './src'),
-//         components: path.resolve(__dirname, './src/components'),
-//         interfaces: path.resolve(__dirname, './src/interfaces/index.tsx'),
-//         pages: path.resolve(__dirname, './src/pages'),
-//         styles: path.resolve(__dirname, './src/styles'),
-//         utils: path.resolve(__dirname, './src/utils'),
-//         hooks: path.resolve(__dirname, './src/hooks'),
-//         custom: path.resolve(__dirname, './custom.d.ts'),
-//         images: path.resolve(__dirname, './public/images'),
-//         data: path.resolve(__dirname, './public/data'),
-//         public: path.resolve(__dirname, './public'),
-//         'config.json': path.resolve(__dirname, './config.json'),
-//         'config.private.json': path.resolve(__dirname, './config.private.json'),
-//       },
-//     },
-//     assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
-//     server: {
-//       port: 9000,
-//     }
-//   };
-// });
