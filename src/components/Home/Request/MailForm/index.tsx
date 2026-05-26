ea724b74461b9894fcab15205ad4172931be920d
MGS-sails <mahfouzshehu@gmail.com>
2026-05-26 17:35:40 +0100

    Fixed linter error

--- src/components/Home/Request/MailForm/index.tsx
+++ src/components/Home/Request/MailForm/index.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import Tooltip from 'components/UI/Tooltip';
 import InfoBanner from 'components/UI/InfoBanner';
 import UserContext, {
   UserDetail,
